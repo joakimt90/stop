bbb76cad7cbf1573b8bb3b5910ed7f88206b56e2
jindexor <joakimt90@gmail.com>
2026-04-03 04:15:35 +0200

    Migrate netlifyBuildv4 from Netlify to Vercel Nitro

--- netlifyBuildv4/.output/server/_tanstack-start-manifest_v-uBbcfCcG.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ "routes": { "__root__": { "filePath": "/Users/joakimthomter/Desktop/9PROJECTS/stop/netlifyBuildv4/src/routes/__root.tsx", "children": ["/"], "assets": [{ "tag": "link", "attrs": { "rel": "stylesheet", "href": "/assets/main-66gdpEEM.css", "type": "text/css" } }], "preloads": ["/assets/main-DXZfc7J5.js"] }, "/": { "filePath": "/Users/joakimthomter/Desktop/9PROJECTS/stop/netlifyBuildv4/src/routes/index.tsx", "assets": [], "preloads": ["/assets/index-_OcwfTW5.js"] } }, "clientEntry": "/assets/main-DXZfc7J5.js" });
-export {
-  tsrStartManifest
-};
